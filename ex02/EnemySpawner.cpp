7c0e95e7878a559dcae8608584572ddacc5640ba
Herovero <herovero0117@gmail.com>
2026-08-09 14:03:49 +0800

    ex02 done

---
+++ ex02/EnemySpawner.cpp
@@ -0,0 +1,14 @@
+#include "EnemySpawner.hpp"
+
+EnemySpawner::EnemySpawner(int maxCount) : _maxCount(maxCount), _spawnedCount(0) {}
+
+int EnemySpawner::spawn() {
+    if (_spawnedCount < _maxCount) {
+        _spawnedCount++;
+    }
+    return _spawnedCount;
+}
+
+int EnemySpawner::getSpawned() const {
+    return _spawnedCount;
+}
---
+++ ex02/EnemySpawner.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class EnemySpawner {
+    private: 
+      int _maxCount;
+      int _spawnedCount;
+
+    public:
+      EnemySpawner(int maxCount);
+
+      int spawn();
+
+      int getSpawned() const;
+  };
